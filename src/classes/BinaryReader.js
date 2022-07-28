1581151d3c0f6d7fd619c7efb9e1c55b5ac362ae
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2022-07-28 19:13:35 -0400

    Finishing BinaryReader class.

--- src/classes/BinaryReader.js
+++ src/classes/BinaryReader.js
@@ -28,36 +28,13 @@ export class BinaryReader
 	}
 
 	/**
-	 * Reads a string's length from the file and then the string itself.
-	 * 
-	 * @returns {String}
-	 */
-	readString() 
-	{
-		let length = this.view.getUint8(this.position);
-
-		this.position += 1;
-
-		let buffer = [];
-
-		for (let i = 0; i < length; i++) 
-		{
-			buffer.push(this.view.getUint8(this.position));
-
-			this.position += 1;
-		}
-	
-		return String.fromCharCode.apply(String, buffer);
-	}
-
-	/**
-	 * Reads an unsigned 8-bit integer from the file.
+	 * Returns a signed 8-bit integer.
 	 * 
 	 * @returns {Number}
 	 */
-	readUInt8()
+	readInt8()
 	{
-		const int = this.view.getUint8(this.position);
+		const int = this.view.getInt8(this.position);
 
 		this.position += 1;
 
@@ -65,7 +42,77 @@ export class BinaryReader
 	}
 
 	/**
-	 * Reads an unsigned 32-bit integer from the file.
+	 * Reads a signed 16-bit integer.
+	 * 
+	 * @returns {Number}
+	 */
+	readInt16()
+	{
+		const int = this.view.getInt16(this.position, true);
+
+		this.position += 2;
+
+		return int;
+	}
+
+	/**
+	 * Reads a signed 32-bit integer.
+	 * 
+	 * @returns {Number}
+	 */
+	readInt32()
+	{
+		const int = this.view.getInt32(this.position, true);
+
+		this.position += 4;
+
+		return int;
+	}
+
+	/**
+	 * Reads a signed 64-bit integer.
+	 * 
+	 * @returns {BigInt}
+	 */
+	readInt64()
+	{
+		const bigInt = this.view.getBigInt64(this.position, true);
+
+		this.position += 8;
+
+		return bigInt;
+	}
+
+	/**
+	 * Reads an unsigned 8-bit integer.
+	 * 
+	 * @returns {Number}
+	 */
+	readUInt8()
+	{
+		const int = this.view.getUint8(this.position, true);
+
+		this.position += 1;
+
+		return int;
+	}
+
+	/**
+	 * Reads an unsigned 16-bit integer.
+	 * 
+	 * @returns {Number}
+	 */
+	readUInt16()
+	{
+		const int = this.view.getUint16(this.position, true);
+
+		this.position += 2;
+
+		return int;
+	}
+
+	/**
+	 * Reads an unsigned 32-bit integer.
 	 * 
 	 * @returns {Number}
 	 */
@@ -76,5 +123,80 @@ export class BinaryReader
 		this.position += 4;
 
 		return int;
+	}
+	
+	/**
+	 * Reads an unsigned 64-bit integer.
+	 * 
+	 * @returns {BigInt}
+	 */
+	readUInt64()
+	{
+		const bigInt = this.view.getBigUint64(this.position, true);
+
+		this.position += 8;
+
+		return bigInt;
+	}
+
+	/**
+	 * Returns a 32-bit floating point number.
+	 * 
+	 * @returns {Number}
+	 */
+	readFloat32()
+	{
+		const float = this.view.getFloat32(this.position, true);
+
+		this.position += 4;
+
+		return float;
+	}
+
+	/**
+	 * Reads a 64-bit floating point number.
+	 * 
+	 * @returns {Number}
+	 */
+	readFloat64()
+	{
+		const float = this.view.getFloat64(this.position, true);
+
+		this.position += 8;
+
+		return float;
+	}
+
+	/**
+	 * Reads a char.
+	 * 
+	 * @returns {String}
+	 */
+	readChar()
+	{
+		const int = this.readUInt8();
+
+		return String.fromCharCode(int);
+	}
+
+	/**
+	 * Reads a string.
+	 * 
+	 * This function assumes the next byte is a string length followed by the bytes of the string.
+	 * 
+	 * @returns {String}
+	 */
+	readString() 
+	{
+		const length = this.readUInt8();
+
+		let buffer = [];
+
+		for (let i = 0; i < length; i++) 
+		{
+			buffer.push(this.readUInt8());
+		}
+	
+		return String.fromCharCode.apply(String, buffer);
 	}
 }
